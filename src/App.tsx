1fa10a0330c8f03f28cb69965f01e40585e853a5
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-03-21 22:29:24 +0100

    configuraciones iniciales terminadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='bg-red-800 text-xl mt-10 p-5 flex justify-center'>
+        Este es un título de prueba 2
+      </h1>
+      <h2 className='bg-blue-600 flex justify-center'>Subtítulo</h2>
     </>
   );
 }
