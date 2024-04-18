26ddc5f4e9b77ddf0c3706c6c5875ad483d35cc1
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-04-18 17:41:34 +0200

    Proyecto terminado, a la espera de que funcione la API para traer las imágenes de los planetas

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -6,14 +6,14 @@ const Character = ({ character }: CharacterPlanetProps) => {
     <>
       <div className=' characterMainContainer flex  flex-col items-center my-6  lg:w-[420px]'>
         <img
-          className='characterImage w-28 mb-6 lg:w-40'
+          className='characterImage w-28 mb-6 lg:w-56 '
           src={character.image}
         />
-        {/* <div className=' characterDescription w-4/6 border p-5'>
-          <p className={'flex justify-center text-gray-500 text-sm'}>
+        <div className='characterDescription hidden w-4/ p-5 xl:block'>
+          <p className={'flex justify-center text-gray-500 text-md'}>
             {character.description}
           </p>
-        </div> */}
+        </div>
       </div>
     </>
   );
